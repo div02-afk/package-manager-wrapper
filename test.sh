2f2ba30ec01f6e72ffbb4fd06a1cec69850ad103
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-11-28 19:05:16 +0530

    code improvements and optimizations

--- test.sh
+++ test.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-
 # Check if Homebrew is installed
 if ! command -v brew &> /dev/null; then
-    #   echo "Homebrew is not installed. Skipping Homebrew installation."
     use_brew=false
 else
     use_brew=true
 fi
 
-
+# Check if apt-get is installed
 if ! command -v apt &> /dev/null; then
-    #   echo "apt-get is not installed. Skipping apt-get installation."
     use_apt=false
 else
     use_apt=true
+fi
+
+if [ "$use_brew" = false ] && [ "$use_apt" = false ]; then
+    echo "Neither Homebrew nor apt is available. Exiting."
+    exit 1
 fi
 
 FILENAME="/usr/package_index.txt"
@@ -24,19 +26,16 @@ fi
 
 function brew_install(){
     if [ "$use_brew" = true ]; then
-        message=$(brew install "$1" 2>&1)
-        echo "$message"
-        if [ $? -eq 0 ]; then
-            add_package "$package" "brew"
-            return 0
+        if brew install "$1"; then
+            add_package "$1" "brew"
         else
-            echo "Failed to install $1 with Homebrew. Searching for package..."
-            
+            echo "Failed to install $1 with Homebrew."
         fi
     else
         return 1
     fi
 }
+
 function apt_install(){
     if [ "$use_apt" = true ]; then
         sudo apt install -y "$1"
@@ -47,15 +46,16 @@ function apt_install(){
             echo "Failed to install $1 with apt. Searching for package..."
             sudo apt search "$1" | head -n 20
         fi
-        return 1;
+        return 1
     else
         return 1
     fi
 }
+
 function brew_remove(){
     if [ "$use_brew" = true ]; then
-        brew uninstall "$1"
-        if [ $? -eq 0 ]; then
+        
+        if brew uninstall "$1"; then
             remove_package "$package"
             return 0
         fi
@@ -66,8 +66,8 @@ function brew_remove(){
 
 function apt_remove(){
     if [ "$use_apt" = true ]; then
-        sudo apt remove "$1"
-        if [ $? -eq 0 ]; then
+        
+        if  sudo apt remove "$1"; then
             remove_package "$package"
             return 0
         fi
@@ -75,6 +75,7 @@ function apt_remove(){
         return 1
     fi
 }
+
 function update(){
     if [ "$use_apt" = true ]; then
         sudo apt update
@@ -90,97 +91,119 @@ function add_package() {
     echo "$package_name : $package_manager" >> "$FILENAME"
     echo "Added $package_name with $package_manager."
 }
+
 function remove_package() {
     local package_name=$1
     if grep -q "^$package_name :" "$FILENAME"; then
-        # Delete the line with the matching package name
-        sed -i "/^$package_name :/d" "$FILENAME"
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            sed -i '' "/^$package_name :/d" "$FILENAME"
+        else
+            sed -i "/^$package_name :/d" "$FILENAME"
+        fi
         echo "Package $package_name removed."
     else
         echo "Package $package_name not found."
     fi
 }
+
 function search_package() {
     local package_name=$1
     local result
     result=$(grep "^$package_name :" "$FILENAME")
     if [ -n "$result" ]; then
-        # Extract the package manager using 'cut' by splitting at the colon
         local package_manager
-        package_manager=$(echo "$result" | cut -d ':' -f 2 | xargs)  # Trim leading/trailing whitespace
-        echo "$package_manager"  # Use echo to return the value
+        package_manager=$(echo "$result" | cut -d ':' -f 2 | xargs)
+        echo "$package_manager"
     else
-        echo ""  # Return an empty string if not found
+        echo ""
     fi
 }
 
 command=$1
 shift
 packages=("$@")
-if [ "$command" = "install" ]; then
-    echo "Packages to install: ${packages[@]}"
-    for package in "${packages[@]}"; do
-        package_manager=$(search_package "$package")
-        echo "Package manager: $package_manager"
-        if [ "$package_manager" = "brew" ]; then
-            echo "Package already installed with Homebrew."
-            continue
-            
-            elif [ "$package_manager" = "apt" ]; then
-            echo "Package already installed with apt."
-            continue
-        fi
-        echo "Installing $package with Homebrew..."
-        brew_install "$package"
-        if [ $? -eq 0 ]; then
-            echo "$package installed successfully with Homebrew."
-        else
-            echo "Failed to install $package with Homebrew. Trying apt..."
-            apt_install "$package"
+
+case "$command" in
+    install)
+        echo "Packages to install: ${packages[@]}"
+        for package in "${packages[@]}"; do
+            package_manager=$(search_package "$package")
+            # echo "Package manager: $package_manager"
+            case "$package_manager" in
+                brew)
+                    echo "Package already installed with Homebrew."
+                    continue
+                ;;
+                apt)
+                    echo "Package already installed with apt."
+                    continue
+                ;;
+            esac
+            echo "Installing $package with Homebrew..."
+            brew_install "$package"
             if [ $? -eq 0 ]; then
-                echo "$package installed successfully with apt."
+                echo "$package installed successfully with Homebrew."
             else
-                echo "Failed to install $package with both Homebrew and apt."
+                echo "Failed to install $package with Homebrew. Trying apt..."
+                apt_install "$package"
+                if [ $? -eq 0 ]; then
+                    echo "$package installed successfully with apt."
+                else
+                    echo "Failed to install $package with both Homebrew and apt."
+                fi
             fi
-        fi
-    done
-    elif [ "$command" = "remove" ]; then
-    for package in "${packages[@]}"; do
-        package_manager=$(search_package "$package")
-        echo "Package manager: $package_manager"
-        if [ "$package_manager" = "brew" ]; then
-            echo "Removing $package with Homebrew..."
-            brew_remove "$package"
-            
-            elif [ "$package_manager" = "apt" ]; then
-            echo "Removing $package with apt..."
-            apt_remove "$package"
-            
-        else
-            echo "Package $package not found."
-        fi
-    done
-    elif [ "$command" = "update" ]; then
-    update
-    elif [ "$command" = "reindex" ]; then
-    rm "$FILENAME"
-    touch "$FILENAME"
-    brew_packages=($(brew list))
-    apt_packages=($(apt list --installed | awk -F/ '{print $1}'))
-    apt_packages=("${apt_packages[@]:1}")
-    total_packages_len=$(( ${#brew_packages[@]} + ${#apt_packages[@]} ))
-    for package in "${brew_packages[@]}"; do
-        add_package "$package" "brew"
-    done
-    for package in "${apt_packages[@]}"; do
-        add_package "$package" "apt"
-    done
-    echo "Reindexed $total_packages_len packages."
-    elif [ "$command" = "search" ]; then
-    search_package "${packages[0]}"
-    elif [ "$command" = "list" ]; then
-    cat "$FILENAME"
-else
-    echo "Unknown command: $command"
-    exit 1
-fi
+        done
+    ;;
+    remove)
+        for package in "${packages[@]}"; do
+            package_manager=$(search_package "$package")
+            # echo "Package manager: $package_manager"
+            case "$package_manager" in
+                brew)
+                    echo "Removing $package with Homebrew..."
+                    brew_remove "$package"
+                ;;
+                apt)
+                    echo "Removing $package with apt..."
+                    apt_remove "$package"
+                ;;
+                *)
+                    echo "Package $package not found."
+                ;;
+            esac
+        done
+    ;;
+    update)
+        update
+    ;;
+    reindex)
+        rm "$FILENAME"
+        touch "$FILENAME"
+        brew_packages=($(brew list))
+        apt_packages=($(apt list --installed | awk -F/ '{print $1}'))
+        apt_packages=("${apt_packages[@]:1}")
+        total_packages_len=$(( ${#brew_packages[@]} + ${#apt_packages[@]} ))
+        (
+            for package in "${brew_packages[@]}"; do
+                add_package "$package" "brew"
+            done
+        ) &
+        (
+            for package in "${apt_packages[@]}"; do
+                add_package "$package" "apt"
+            done
+        ) &
+        wait
+        echo "Reindexed $total_packages_len packages."
+    ;;
+    search)
+        search_package "${packages[0]}"
+    ;;
+    list)
+        cat "$FILENAME"
+    ;;
+    *)
+        echo "Unknown command: $command"
+        exit 1
+    ;;
+esac
